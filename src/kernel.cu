95396f621d39126fe4ec0f7a0b44ed5b2646b067
Jose A. Henriquez <josearmando.henriquez@gmail.com>
2021-12-03 11:33:33 +0100

    refactor(kernel): moved kernels to separate file

--- src/kernel.cu
+++ src/kernel.cu
@@ -8,69 +8,6 @@ idx(const uint8_t* a, const unsigned int width, int x, int y) {
 }
 
 __global__ void
-sobelImage(const uint8_t* a,
-           const unsigned int w,
-           const unsigned int h,
-           uint32_t* sx,
-           uint32_t* sy) {
-  int x = threadIdx.x + blockIdx.x * blockDim.x;
-  int y = threadIdx.y + blockIdx.y * blockDim.y;
-  if (x > 0 && y > 0 && x < w - 1 && y < h - 1) {
-    sx[y * w + x] =
-        abs(-1 * idx(a, w, x - 1, y - 1) + -2 * idx(a, w, x - 1, y) +
-            -1 * idx(a, w, x - 1, y + 1) + idx(a, w, x + 1, y - 1) +
-            2 * idx(a, w, x + 1, y) + idx(a, w, x + 1, y + 1));
-    sy[y * w + x] =
-        abs(idx(a, w, x - 1, y - 1) + 2 * idx(a, w, x, y - 1) +
-            idx(a, w, x + 1, y - 1) + -1 * idx(a, w, x - 1, y + 1) +
-            -2 * idx(a, w, x, y + 1) + -1 * idx(a, w, x + 1, y + 1));
-  }
-}
-
-
-__global__ void
-addToPool(const uint32_t* sobelX,
-          const uint32_t* sobelY,
-          const unsigned int width,
-          const unsigned int height,
-          const unsigned int poolSize,
-          const unsigned int pooledWidth,
-          uint32_t* pooledX,
-          uint32_t* pooledY) {
-  int x = threadIdx.x + blockIdx.x * blockDim.x;
-  int y = threadIdx.y + blockIdx.y * blockDim.y;
-  int px = x / poolSize;
-  int py = y / poolSize;
-
-  atomicAdd(pooledX + px + py * pooledWidth, *(sobelX + x + y * width));
-  atomicAdd(pooledY + px + py * pooledWidth, *(sobelY + x + y * width));
-}
-
-__global__ void
-averagePool(const unsigned int poolSize,
-            const unsigned int pooledWidth,
-            uint32_t* pooledX,
-            uint32_t* pooledY) {
-  int x = threadIdx.x + blockIdx.x * blockDim.x;
-  int y = threadIdx.y + blockIdx.y * blockDim.y;
-  *(pooledX + x + y * pooledWidth) /= poolSize;
-  *(pooledY + x + y * pooledWidth) /= poolSize;
-}
-
-__global__ void
-computeResponse(const uint32_t* pooledX,
-                const uint32_t* pooledY,
-                const unsigned int pooledWidth,
-                uint32_t* response) {
-  int x = threadIdx.x + blockIdx.x * blockDim.x;
-  int y = threadIdx.y + blockIdx.y * blockDim.y;
-
-  int d = *(pooledX + x + y * pooledWidth) - *(pooledY + x + y * pooledWidth);
-
-  *(response + x + y * pooledWidth) = d < 0 ? 0 : d;
-}
-
-__global__ void
 grayscale(const uint8_t* orig, const unsigned int width, uint8_t* gray) {
   int x = threadIdx.x + blockIdx.x * blockDim.x;
   int y = threadIdx.y + blockIdx.y * blockDim.y;
@@ -79,17 +16,62 @@ grayscale(const uint8_t* orig, const unsigned int width, uint8_t* gray) {
   *(gray + x + y * width) = (*p + *(p + 1) + *(p + 2)) / 3;
 }
 
+// Computes the vertical and horizontal sobels and pools the image.
+__global__ void
+sobelImage(const uint8_t* a,
+           const unsigned int w,
+           const unsigned int h,
+           const unsigned int poolSize,
+           const unsigned int pooledWidth,
+           uint32_t* pooledX,
+           uint32_t* pooledY) {
+  int x = threadIdx.x + blockIdx.x * blockDim.x;
+  int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+  if (x > 0 && y > 0 && x < w - 1 && y < h - 1) {
+    int px = x / poolSize;
+    int py = y / poolSize;
+
+    int dx =
+        abs(-1 * idx(a, w, x - 1, y - 1) + -2 * idx(a, w, x - 1, y) +
+            -1 * idx(a, w, x - 1, y + 1) + idx(a, w, x + 1, y - 1) +
+            2 * idx(a, w, x + 1, y) + idx(a, w, x + 1, y + 1));
+    int dy =
+        abs(idx(a, w, x - 1, y - 1) + 2 * idx(a, w, x, y - 1) +
+            idx(a, w, x + 1, y - 1) + -1 * idx(a, w, x - 1, y + 1) +
+            -2 * idx(a, w, x, y + 1) + -1 * idx(a, w, x + 1, y + 1));
+
+    atomicAdd(pooledX + px + py * pooledWidth, dx);
+    atomicAdd(pooledY + px + py * pooledWidth, dy);
+  }
+}
+
+// Averages the pool values and computes the response.
+__global__ void
+computeResponse(const uint32_t* pooledY,
+                const unsigned int poolSize,
+                const unsigned int pooledWidth,
+                uint32_t* pooledX) {
+  int x = threadIdx.x + blockIdx.x * blockDim.x;
+  int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+  int d = *(pooledX + x + y * pooledWidth) / poolSize -
+          *(pooledY + x + y * pooledWidth) / poolSize;
+
+  *(pooledX + x + y * pooledWidth) = d < 0 ? 0 : d;
+}
+
 __global__ void
 erode(const uint32_t* response,
       const unsigned int width,
       const unsigned int height,
       const int kernelHalfWidth,
       const int kernelHalfHeight,
-      uint32_t* eroded) {
+      uint8_t* eroded) {
   int x = threadIdx.x + blockIdx.x * blockDim.x;
   int y = threadIdx.y + blockIdx.y * blockDim.y;
 
-  uint32_t minVal = UINT32_MAX;
+  uint8_t minVal = UINT8_MAX;
   for (int i = -1 * kernelHalfWidth; i < kernelHalfWidth; i++) {
     for (int j = -1 * kernelHalfHeight; j < kernelHalfHeight; j++) {
       int resx = x + i;
@@ -100,22 +82,22 @@ erode(const uint32_t* response,
     }
   }
 
-  if (minVal != UINT32_MAX) {
+  if (minVal != UINT8_MAX) {
     *(eroded + x + y * width) = minVal;
   }
 }
 
 __global__ void
-dilatate(const uint32_t* eroded,
+dilatate(const uint8_t* eroded,
          const unsigned int width,
          const unsigned int height,
          const int kernelHalfWidth,
          const int kernelHalfHeight,
-         uint32_t* dilatated) {
+         uint8_t* dilatated) {
   int x = threadIdx.x + blockIdx.x * blockDim.x;
   int y = threadIdx.y + blockIdx.y * blockDim.y;
 
-  uint32_t maxVal = 0;
+  uint8_t maxVal = 0;
   for (int i = -1 * kernelHalfWidth; i < kernelHalfWidth; i++) {
     for (int j = -1 * kernelHalfHeight; j < kernelHalfHeight; j++) {
       int resx = x + i;
@@ -130,13 +112,12 @@ dilatate(const uint32_t* eroded,
 }
 
 __global__ void
-threshold(const uint32_t* dilatated,
-          const int width,
+threshold(const int width,
           const int maxVal,
-          uint32_t* result) {
+          uint8_t* dilatated) {
   int x = threadIdx.x + blockIdx.x * blockDim.x;
   int y = threadIdx.y + blockIdx.y * blockDim.y;
 
-  *(result + x + y * width) =
-      *(dilatated + x + y * width) > 0.5 * maxVal ? UINT32_MAX : 0;
+  *(dilatated + x + y * width) =
+      *(dilatated + x + y * width) > 0.5 * maxVal ? UINT8_MAX : 0;
 }
--- src/main.cu
+++ src/main.cu
@@ -10,7 +10,13 @@
 
 #include "kernel.cuh"
 
-#define GRIDVAL 20.0
+#define GRID_SIZE 32
+
+#define POOL_SIZE 3
+#define ERO_KERNEL_WIDTH 6
+#define ERO_KERNEL_HEIGHT 4
+#define DIL_KERNEL_WIDTH 25
+#define DIL_KERNEL_HEIGHT 15
 
 unsigned int
 divUp(const unsigned int& a, const unsigned int& b) {
@@ -21,71 +27,54 @@ divUp(const unsigned int& a, const unsigned int& b) {
   }
 }
 
+uint8_t
+getMaxVal(const uint8_t* arr,
+          const unsigned int pooledWidth,
+          const unsigned int pooledHeight) {
+  uint8_t maxVal = 0;
+  for (int i = 0; i < pooledHeight * pooledWidth; i++) {
+    uint8_t curVal = *(arr + i);
+    if (maxVal < curVal) {
+      maxVal = curVal;
+    }
+  }
+
+  return maxVal;
+}
+
 int
 main(int argc, char** argv) {
   if (argc != 3) {
     std::cerr << "Usage: gpgpu <src-image.jpb> <dst-image.png>" << std::endl;
     exit(1);
   }
-  
+
   int width, height, bpp;
 
   uint8_t* rgb = stbi_load(argv[1], &width, &height, &bpp, 3);
 
   int imageSize = sizeof(uint8_t) * (width * height);
-  uint8_t* grayHost = (uint8_t*)malloc(imageSize);
 
   uint8_t* origDev;
   uint8_t* grayDev;
   cudaMalloc((void**)&origDev, imageSize * 3);
   cudaMalloc((void**)&grayDev, imageSize);
   cudaMemcpy(origDev, rgb, imageSize * 3, cudaMemcpyHostToDevice);
-  cudaMemcpy(grayDev, grayHost, imageSize, cudaMemcpyHostToDevice);
 
-  dim3 threadsPerBlock(GRIDVAL, GRIDVAL);
-  dim3 numBlocks(divUp(width, GRIDVAL), divUp(height, GRIDVAL));
+  dim3 threadsPerBlock(GRID_SIZE, GRID_SIZE);
+  dim3 numBlocks(divUp(width, GRID_SIZE), divUp(height, GRID_SIZE));
 
+  // Compute the grayscale.
   grayscale<<<numBlocks, threadsPerBlock>>>(origDev, width, grayDev);
 
   stbi_image_free(rgb);
 
   cudaDeviceSynchronize();
 
-  // TODO: start remove
-  // cudaMemcpy(grayHost, grayDev, imageSize, cudaMemcpyDeviceToHost);
-  // stbi_write_png("etc/data/grayscale.png", width, height, 1, grayHost, width);
-  // TODO: end remove
+  cudaFree(origDev);
 
-  int sobelImageSize = sizeof(uint32_t) * (width * height);
-
-  uint32_t* sobelXDev;
-  cudaMalloc((void**)&sobelXDev, sobelImageSize);
-  uint32_t* sobelYDev;
-  cudaMalloc((void**)&sobelYDev, sobelImageSize);
-
-  sobelImage<<<numBlocks, threadsPerBlock>>>(
-      grayDev, width, height, sobelXDev, sobelYDev);
-
-  cudaDeviceSynchronize();
-
-  // TODO: start remove
-  // uint32_t* sobelXHost = (uint32_t*)malloc(sobelImageSize);
-  // uint32_t* sobelYHost = (uint32_t*)malloc(sobelImageSize);
-  // cudaMemcpy(sobelYHost, sobelYDev, sobelImageSize, cudaMemcpyDeviceToHost);
-  // cudaMemcpy(sobelXHost, sobelXDev, sobelImageSize, cudaMemcpyDeviceToHost);
-  // uint8_t* sobelYHost8 = (uint8_t*)malloc(imageSize);
-  // uint8_t* sobelXHost8 = (uint8_t*)malloc(imageSize);
-  // for (int i = 0; i < width * height; i++) {
-  //   *(sobelXHost8 + i) = *(sobelXHost + i);
-  //   *(sobelYHost8 + i) = *(sobelYHost + i);
-  // }
-  // stbi_write_png("etc/data/sobelx.png", width, height, 1, sobelXHost8, width);
-  // stbi_write_png("etc/data/sobely.png", width, height, 1, sobelYHost8, width);
-  // TODO: end remove
-
-  int poolSize = 3;
-  int pooledWidth = width / poolSize;
-  int pooledHeight = height / poolSize;
+  int pooledWidth = width / POOL_SIZE;
+  int pooledHeight = height / POOL_SIZE;
   int pooledImageSize = sizeof(uint32_t) * (pooledWidth * pooledHeight);
 
   uint32_t* pooledXDev;
@@ -95,163 +84,74 @@ main(int argc, char** argv) {
   cudaMemset(pooledXDev, 0, pooledImageSize);
   cudaMemset(pooledYDev, 0, pooledImageSize);
 
-  addToPool<<<numBlocks, threadsPerBlock>>>(sobelXDev,
-                                            sobelYDev,
-                                            width,
-                                            height,
-                                            poolSize,
-                                            pooledWidth,
-                                            pooledXDev,
-                                            pooledYDev);
-
-  // cudaDeviceSynchronize();
-
-  dim3 threadsPerBlockPool(GRIDVAL, GRIDVAL);
-  dim3 numBlocksPool(divUp(pooledWidth, GRIDVAL), divUp(pooledHeight, GRIDVAL));
-
-  averagePool<<<pooledWidth * pooledHeight, 1>>>(
-      poolSize, pooledWidth, pooledXDev, pooledYDev);
+  // Sobel and add to pool.
+  sobelImage<<<numBlocks, threadsPerBlock>>>(
+      grayDev, width, height, POOL_SIZE, pooledWidth, pooledXDev, pooledYDev);
 
   cudaDeviceSynchronize();
 
-  // TODO: start remove
-  // uint32_t* pooledXHost = (uint32_t*)malloc(pooledImageSize);
-  // uint32_t* pooledYHost = (uint32_t*)malloc(pooledImageSize);
-  // cudaMemcpy(pooledXHost, pooledXDev, pooledImageSize, cudaMemcpyDeviceToHost);
-  // cudaMemcpy(pooledYHost, pooledYDev, pooledImageSize, cudaMemcpyDeviceToHost);
-  // uint8_t* pooledXHost8 = (uint8_t*)malloc(pooledImageSize);
-  // uint8_t* pooledYHost8 = (uint8_t*)malloc(pooledImageSize);
-  // for (int i = 0; i < pooledWidth * pooledHeight; i++) {
-  //   *(pooledXHost8 + i) = *(pooledXHost + i);
-  //   *(pooledYHost8 + i) = *(pooledYHost + i);
-  // }
-  // stbi_write_png("etc/data/pooledx.png",
-  //                pooledWidth,
-  //                pooledHeight,
-  //                1,
-  //                pooledXHost8,
-  //                pooledWidth);
-  // stbi_write_png("etc/data/pooledy.png",
-  //                pooledWidth,
-  //                pooledHeight,
-  //                1,
-  //                pooledYHost8,
-  //                pooledWidth);
-  // TODO: end remove
-
-  uint32_t* responseDev;
-  cudaMalloc((void**)&responseDev, pooledImageSize);
+  cudaFree(grayDev);
+  
+  dim3 threadsPerBlockPool(GRID_SIZE, GRID_SIZE);
+  dim3 numBlocksPool(divUp(pooledWidth, GRID_SIZE),
+                     divUp(pooledHeight, GRID_SIZE));
 
   computeResponse<<<numBlocksPool, threadsPerBlockPool>>>(
-      pooledXDev, pooledYDev, pooledWidth, responseDev);
+      pooledYDev, POOL_SIZE, pooledWidth, pooledXDev);
 
   cudaDeviceSynchronize();
 
-  // TODO: start remove
-  // uint32_t* responseHost = (uint32_t*)malloc(pooledImageSize);
-  // cudaMemcpy(
-  //     responseHost, responseDev, pooledImageSize, cudaMemcpyDeviceToHost);
-  // uint8_t* responseHost8 = (uint8_t*)malloc(pooledImageSize);
-  // for (int i = 0; i < pooledWidth * pooledHeight; i++) {
-  //   *(responseHost8 + i) = *(responseHost + i);
-  // }
-  // stbi_write_png("etc/data/response.png",
-  //                pooledWidth,
-  //                pooledHeight,
-  //                1,
-  //                responseHost8,
-  //                pooledWidth);
-  // TODO: end remove
-
-  int eroKernelWidth = 6;
-  int eroKernelHeight = 3;
-
-  uint32_t* erodedDev;
+  cudaFree(pooledYDev);
+  
+  uint8_t* erodedDev;
   cudaMalloc((void**)&erodedDev, pooledImageSize);
 
-  erode<<<numBlocksPool, threadsPerBlockPool>>>(responseDev,
+  // Erode the image.
+  erode<<<numBlocksPool, threadsPerBlockPool>>>(pooledXDev,
                                                 pooledWidth,
                                                 pooledHeight,
-                                                eroKernelWidth / 2,
-                                                eroKernelHeight / 2,
+                                                ERO_KERNEL_WIDTH / 2,
+                                                ERO_KERNEL_HEIGHT / 2,
                                                 erodedDev);
 
   cudaDeviceSynchronize();
 
-  // TODO: start remove
-  // uint32_t* erodedHost = (uint32_t*)malloc(pooledImageSize);
-  // cudaMemcpy(erodedHost, erodedDev, pooledImageSize, cudaMemcpyDeviceToHost);
-  // uint8_t* erodedHost8 = (uint8_t*)malloc(pooledImageSize);
-  // for (int i = 0; i < pooledWidth * pooledHeight; i++) {
-  //   *(erodedHost8 + i) = *(erodedHost + i);
-  // }
-  // stbi_write_png("etc/data/eroded.png",
-  //                pooledWidth,
-  //                pooledHeight,
-  //                1,
-  //                erodedHost8,
-  //                pooledWidth);
-  // TODO: end remove
+  cudaFree(pooledXDev);
 
-  int dilKernelWidth = 25;
-  int dilKernelHeight = 15;
-  
-  uint32_t* dilatatedDev;
+  uint8_t* dilatatedDev;
   cudaMalloc((void**)&dilatatedDev, pooledImageSize);
 
+  // Dilatate the image.
   dilatate<<<numBlocksPool, threadsPerBlockPool>>>(erodedDev,
                                                    pooledWidth,
                                                    pooledHeight,
-                                                   dilKernelWidth / 2,
-                                                   dilKernelHeight / 2,
+                                                   DIL_KERNEL_WIDTH / 2,
+                                                   DIL_KERNEL_HEIGHT / 2,
                                                    dilatatedDev);
 
   cudaDeviceSynchronize();
 
-  uint32_t* dilatatedHost = (uint32_t*)malloc(pooledImageSize);
+  cudaFree(erodedDev);
+  
+  uint8_t* dilatatedHost = (uint8_t*)malloc(pooledImageSize);
   cudaMemcpy(
       dilatatedHost, dilatatedDev, pooledImageSize, cudaMemcpyDeviceToHost);
 
-  // TODO: start remove
-  // uint8_t* dilatatedHost8 = (uint8_t*)malloc(pooledImageSize);
-  // for (int i = 0; i < pooledWidth * pooledHeight; i++) {
-  //   *(dilatatedHost8 + i) = *(dilatatedHost + i);
-  // }
-  // stbi_write_png("etc/data/dilatated.png",
-  //                pooledWidth,
-  //                pooledHeight,
-  //                1,
-  //                dilatatedHost8,
-  //                pooledWidth);
-  // TODO: end remove
+  uint8_t maxVal = getMaxVal(dilatatedHost, pooledWidth, pooledHeight);
 
-  uint32_t maxVal = 0;
-  for (int i = 0; i < pooledHeight * pooledWidth; i++) {
-    uint32_t curVal = *(dilatatedHost + i);
-    if (maxVal < curVal) {
-      maxVal = curVal;
-    }
-  }
-
-  uint32_t* resultDev;
-  cudaMalloc((void**)&resultDev, pooledImageSize);
-
+  // Threshold the image by 50%.
   threshold<<<numBlocksPool, threadsPerBlockPool>>>(
-      dilatatedDev, pooledWidth, maxVal, resultDev);
+      pooledWidth, maxVal, dilatatedDev);
 
-  // TODO: start remove
-  uint32_t* resultHost = (uint32_t*)malloc(pooledImageSize);
-  cudaMemcpy(resultHost, resultDev, pooledImageSize, cudaMemcpyDeviceToHost);
+  cudaDeviceSynchronize();
+  
+  cudaMemcpy(
+      dilatatedHost, dilatatedDev, pooledImageSize, cudaMemcpyDeviceToHost);
 
-  uint8_t* resultHost8 = (uint8_t*)malloc(pooledImageSize);
-  for (int i = 0; i < pooledWidth * pooledHeight; i++) {
-    *(resultHost8 + i) = *(resultHost + i);
-  }
-  stbi_write_png(argv[2],
-                 pooledWidth,
-                 pooledHeight,
-                 1,
-                 resultHost8,
-                 pooledWidth);
-  // TODO: end remove
+  cudaFree(dilatatedDev);
+  
+  stbi_write_png(
+      argv[2], pooledWidth, pooledHeight, 1, dilatatedHost, pooledWidth);
+
+  free(dilatatedHost);
 }
